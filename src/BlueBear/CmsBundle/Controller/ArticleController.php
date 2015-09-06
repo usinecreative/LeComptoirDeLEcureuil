c9704d8fa2cc51237904ce1ce4b3cdd10923ba94
johnkrovitch <arnaudfrezet@gmail.com>
2015-09-06 23:33:41 +0200

    adding preview actions in menu

---
+++ src/BlueBear/CmsBundle/Controller/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BlueBear\CmsBundle\Controller;
+
+use BlueBear\BaseBundle\Behavior\ControllerTrait;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ArticleController extends Controller
+{
+    use ControllerTrait;
+
+    /**
+     * @Template()
+     */
+    public function previewAction()
+    {
+        return [];
+    }
+}
