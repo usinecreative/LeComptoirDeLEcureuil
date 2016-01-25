254f7c1d08912b899f8595c7fbe596c2cfb7b42b
johnkrovitch <arnaudfrezet@gmail.com>
2016-01-25 23:12:33 +0100

    Improve contact page styles, add translations, improve main menu dropdown colors

--- src/LeComptoirDeLEcureuil/FrontBundle/Form/Type/ContactType.php
+++ src/LeComptoirDeLEcureuil/FrontBundle/Form/Type/ContactType.php
@@ -10,14 +10,20 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
+            ->add('firstName', 'text', [
+                'label' => 'lecomptoir.contact.first_name'
+            ])
+            ->add('lastName', 'text', [
+                'label' => 'lecomptoir.contact.last_name'
+            ])
             ->add('email', 'email')
             // anti spam field
             ->add('url', 'text', [
                 'required' => false
             ])
-            ->add('message', 'textarea')
+            ->add('message', 'textarea', [
+                'label' => 'lecomptoir.contact.message'
+            ])
         ;
     }
 
