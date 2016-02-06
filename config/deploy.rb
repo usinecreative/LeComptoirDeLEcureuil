a92118992b2e78b0d3f067b19e2ca99dd46b8f15
johnkrovitch <arnaudfrezet@gmail.com>
2016-02-07 00:59:22 +0100

    Fix new app.php for Symfony 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :web_path,        "web"
 set :log_path,        "var/logs"
 set :cache_path,      "var/cache"
 set :app_config_path, fetch(:app_path) + "/config"
-set :symfony_console_path, fetch(:app_path) + "/console"
+set :symfony_console_path, "bin/console"
 set :controllers_to_clear, ["app_*.php"]
 set :symfony_env,  "prod"
 
