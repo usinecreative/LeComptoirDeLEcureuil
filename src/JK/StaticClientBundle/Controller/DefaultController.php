6458a3e36e668d650229c95bc1685da12b335bda
Arnaud Frézet <arnaudfrezet@gmail.com>
2016-02-12 17:07:54 +0100

    Add JK static bundle

---
+++ src/JK/StaticClientBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace JK\StaticClientBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('JKStaticClientBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/JK/StaticClientBundle/JKStaticClientBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace JK\StaticClientBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JKStaticClientBundle extends Bundle
+{
+}
