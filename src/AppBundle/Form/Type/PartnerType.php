9f4efa60156d3a97244ec88e59112eb99873de2e
johnkrovitch <arnaudfrezet@gmail.com>
2016-07-11 22:49:03 +0200

    Add Partners CRUD

---
+++ src/AppBundle/Form/Type/PartnerType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class PartnerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'partner.name'
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'rows' => 10,
+                    'class' => 'tinymce',
+                    'data-theme' => 'advanced'
+                ],
+                'label' => 'partner.description'
+            ])
+            ->add('website', UrlType::class, [
+                'label' => 'partner.website'
+            ])
+        ;
+    }
+}
