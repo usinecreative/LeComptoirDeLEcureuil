2dcd456d4b847bbd2e76142c1c814d773daa83d5
Arnaud Frézet <arnaudfrezet@gmail.com>
2016-08-26 12:23:30 +0200

    Add image gallery in tinyMce popup

---
+++ src/BlueBear/CmsBundle/Controller/ImageController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace BlueBear\CmsBundle\Controller;
+
+use LAG\AdminBundle\Controller\CRUDController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ImageController extends CRUDController
+{
+    /**
+     * @Template()
+     */
+    public function galleryDialogAction()
+    {
+
+    }
+}
