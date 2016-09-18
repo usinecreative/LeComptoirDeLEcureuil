41c932ad06103f2a8484f721891356205c28cae4
Arnaud Frézet <arnaudfrezet@gmail.com>
2016-09-18 18:55:15 +0200

    remove unused imports

--- src/BlueBear/CmsBundle/Form/Type/CategoryType.php
+++ src/BlueBear/CmsBundle/Form/Type/CategoryType.php
@@ -2,7 +2,6 @@
 
 namespace BlueBear\CmsBundle\Form\Type;
 
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
