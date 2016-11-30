faa32df9765d1e8a0bce00517a12cf03adb99ded
Arnaud Frézet <arnaudfrezet@gmail.com>
2016-11-30 00:19:58 +0000

    add a media interface

---
+++ src/JK/CmsBundle/Entity/MediaInterface.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace JK\CmsBundle\Entity;
+
+/**
+ * A media handled by the CMS should implements this interface.
+ */
+interface MediaInterface
+{
+    /**
+     * Define the media name.
+     *
+     * @param string $name
+     */
+    public function setName($name);
+
+    /**
+     * Define the media file name.
+     *
+     * @param string $fileName
+     */
+    public function setFileName($fileName);
+
+    /**
+     * Define the media file type (images, audio, video...).
+     *
+     * @param string $fileType
+     */
+    public function setFileType($fileType);
+
+    /**
+     * Define the media type.
+     *
+     * @param string $type
+     */
+    public function setType($type);
+
+    /**
+     * Define the media file size.
+     *
+     * @param int $size
+     */
+    public function setSize($size);
+
+    /**
+     * Return the media name.
+     *
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * Return the media file name.
+     *
+     * @return string
+     */
+    public function getFileName();
+
+    /**
+     * Return the media file type.
+     *
+     * @return string
+     */
+    public function getFileType();
+
+    /**
+     * Return the media file type.
+     *
+     * @return string
+     */
+    public function getType();
+
+    /**
+     * Return the media file size.
+     *
+     * @return int
+     */
+    public function getSize();
+}
