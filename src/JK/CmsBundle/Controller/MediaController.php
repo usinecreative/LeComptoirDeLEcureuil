83f0aca94f28f17910229789dcd1ecbabec29382
Arnaud Frézet <arnaudfrezet@gmail.com>
2016-12-19 00:12:01 +0100

    add the MediaController to handle gallery and list

--- src/JK/CmsBundle/Controller/MediaController.php
+++ src/JK/CmsBundle/Controller/MediaController.php
@@ -2,11 +2,19 @@
 
 namespace JK\CmsBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use LAG\AdminBundle\Admin\AdminInterface;
+use LAG\AdminBundle\Controller\CRUDController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class MediaController extends Controller
+class MediaController extends CRUDController
 {
+    /**
+     * Return the Media gallery for the modal (no layout used).
+     *
+     * @return Response
+     */
     public function modalGalleryAction()
     {
         $mediaList = $this
@@ -17,7 +25,14 @@ class MediaController extends Controller
             'mediaList' => $mediaList
         ]);
     }
-
+    
+    /**
+     * Return the content that should be inserted into a tinyMce instance.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
     public function galleryContentAction(Request $request)
     {
         $idsString = $request->get('ids', '');
@@ -32,5 +47,94 @@ class MediaController extends Controller
         return $this->render('@JKCms/Media/galleryContent.html.twig', [
             'mediaList' => $mediaList
         ]);
+    }
+    
+    /**
+     * Display the Media list for the Admin.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function listAction(Request $request)
+    {
+        // TODO remove this method when AdminBundle can handle template configuration (v0.5)
+        $viewParameters = parent::listAction($request);
+        /** @var AdminInterface $admin */
+        $admin = $viewParameters['admin'];
+        $viewParameters['mediaEditAction'] = $admin->generateRouteName('edit');
+    
+        return $this->render('@JKCms/Media/list.html.twig', $viewParameters);
+    }
+    
+    /**
+     * Edit a Media.
+     *
+     * @param Request $request
+     *
+     * @return array|RedirectResponse|Response
+     */
+    public function editAction(Request $request)
+    {
+        // TODO remove this method when AdminBundle can handle template configuration (v0.5)
+        $viewParameters = parent::editAction($request);
+    
+        if ($viewParameters instanceof Response) {
+            return $viewParameters;
+        }
+        $viewParameters['linkedArticles'] = '';
+    
+        return $this->render('@JKCms/Media/edit.html.twig', $viewParameters);
+    }
+    
+    /**
+     * Create a new Media.
+     *
+     * @param Request $request
+     *
+     * @return RedirectResponse|Response
+     */
+    public function createAction(Request $request)
+    {
+        $admin = $this->getAdminFromRequest($request);
+        $admin->handleRequest($request, $this->getUser());
+    
+        // check permissions
+        $this->forward404IfNotAllowed($admin);
+    
+        // create form
+        $formType = $admin
+            ->getConfiguration()
+            ->getParameter('form');
+        $form = $this->createForm($formType, $admin->create(), [
+            'is_file_required' => true,
+        ]);
+        $form->handleRequest($request);
+    
+        if ($form->isValid()) {
+            // save entity
+            $success = $admin->save();
+        
+            if ($success) {
+                // if save is pressed, user stay on the edit view
+                if ($request->request->get('submit') == 'save') {
+                    $editRoute = $admin->generateRouteName('edit');
+                
+                    return $this->redirectToRoute($editRoute, [
+                        'id' => $admin->getUniqueEntity()->getId(),
+                    ]);
+                } else {
+                    // otherwise user is redirected to list view
+                    $listRoute = $admin->generateRouteName('list');
+                
+                    return $this->redirectToRoute($listRoute);
+                }
+            }
+        }
+        return $this->render('@JKCms/Media/create.html.twig', [
+            'admin' => $admin,
+            'form' => $form->createView(),
+        ]);
+        
     }
 }
