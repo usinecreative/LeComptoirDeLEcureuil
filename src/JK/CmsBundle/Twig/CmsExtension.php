b0312bcca5e7de60b018388fa298ae6f479acc73
Arnaud Frézet <arnaudfrezet@gmail.com>
2016-12-19 00:13:16 +0100

    add CMS Twig extension to access AssetsHelper in template

--- src/JK/CmsBundle/Twig/CmsExtension.php
+++ src/JK/CmsBundle/Twig/CmsExtension.php
@@ -2,7 +2,6 @@
 
 namespace JK\CmsBundle\Twig;
 
-use Exception;
 use JK\CmsBundle\Assets\AssetsHelper;
 use JK\CmsBundle\Entity\MediaInterface;
 use Twig_Extension;
@@ -29,6 +28,8 @@ class CmsExtension extends Twig_Extension
     }
     
     /**
+     * Return the Twig function mapping.
+     *
      * @return array
      */
     public function getFunctions()
@@ -36,33 +37,59 @@ class CmsExtension extends Twig_Extension
         return [
             new Twig_SimpleFunction('cms_media_path', [$this, 'cmsMediaPath']),
             new Twig_SimpleFunction('cms_media_directory', [$this, 'cmsMediaDirectory']),
+            new Twig_SimpleFunction('cms_media_size', [$this, 'cmsMediaSize']),
         ];
     }
-
+    
     /**
      * Return the path to an media according to its type.
      *
      * @param MediaInterface $media
+     * @param bool $absolute
+     * @param bool $cache
+     * @param null|string $mediaFilter
+     *
      * @return string
-     * @throws Exception
      */
-    public function cmsMediaPath(MediaInterface $media)
+    public function cmsMediaPath(MediaInterface $media, $absolute = true, $cache = true, $mediaFilter = null)
     {
         return $this
             ->assetsHelper
-            ->getMediaPath($media);
+            ->getMediaPath($media, $absolute, $cache, $mediaFilter);
     }
     
     /**
      * Return the media web directory according to its type and the mapping.
      *
-     * @param $mappingName
-     * @return mixed
+     * @param string $mappingName
+     *
+     * @return string
      */
     public function cmsMediaDirectory($mappingName)
     {
         return $this
             ->assetsHelper
             ->getMediaDirectory($mappingName);
+    }
+    
+    /**
+     * Return a string representing the media size in the most readable unit.
+     *
+     * @param MediaInterface $media
+     * @return string
+     */
+    public function cmsMediaSize(MediaInterface $media)
+    {
+        $size = $media->getSize();
+        // try size in Kio
+        $size = round($size / 1024, 2);
+    
+        if ($size >= 1000) {
+            $size = round($size / 1024, 2);
+    
+            return $size.' Mo';
+        }
+    
+        return $size.' Ko';
     }
 }
