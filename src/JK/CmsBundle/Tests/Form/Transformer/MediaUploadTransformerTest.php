2013f73496e715079cc5a699be409b6d7c61f413
Arnaud Frézet <arnaudfrezet@gmail.com>
2016-12-18 22:35:37 +0100

    add tests for the MediaUploadTransformer

--- src/JK/CmsBundle/Tests/Form/Transformer/MediaUploadTransformerTest.php
+++ src/JK/CmsBundle/Tests/Form/Transformer/MediaUploadTransformerTest.php
@@ -3,25 +3,25 @@
 namespace JK\CmsBundle\Tests\Form\Transformer;
 
 use JK\CmsBundle\Entity\Media;
-use JK\CmsBundle\Form\Transformer\MediaTransformer;
+use JK\CmsBundle\Form\Transformer\MediaUploadTransformer;
 use JK\CmsBundle\Repository\MediaRepository;
 use PHPUnit_Framework_TestCase;
 
-class MediaTransformerTest extends PHPUnit_Framework_TestCase
+class MediaUploadTransformerTest extends PHPUnit_Framework_TestCase
 {
     public function testTransform()
     {
         $media = new Media();
         $media->setId(666);
         $media->setFileName('test.devil');
-        
+        /** @var MediaRepository|\PHPUnit_Framework_MockObject_MockObject $repository */
         $repository = $this->getMock(MediaRepository::class, [], [], '', false);
         
         $repository
             ->method('find')
             ->willReturn($media);
         
-        $transformer = new MediaTransformer($repository);
+        $transformer = new MediaUploadTransformer($repository);
         $data = $transformer->transform($media);
     
         $this->assertEquals(666, $data['id']);
