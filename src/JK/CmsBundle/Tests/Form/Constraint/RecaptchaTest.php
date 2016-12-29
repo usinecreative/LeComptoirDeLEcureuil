a53b9d2172d8d30613a9492585279cabb0cb62d2
Arnaud Frézet <arnaudfrezet@gmail.com>
2016-12-29 11:41:20 +0100

    add Recaptcha form constraint

---
+++ src/JK/CmsBundle/Tests/Form/Constraint/RecaptchaTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace JK\CmsBundle\Tests\Form\Constraint;
+
+use JK\CmsBundle\Form\Constraint\Recaptcha;
+use JK\CmsBundle\Form\Validator\RecaptchaValidator;
+use LAG\AdminBundle\Tests\AdminTestBase;
+use Symfony\Component\Validator\Constraint;
+
+class RecaptchaTest extends AdminTestBase
+{
+    public function testValidatedBy()
+    {
+        $constraint = new Recaptcha();
+        $this->assertEquals(RecaptchaValidator::class, $constraint->validatedBy());
+    }
+    
+    public function testGetTargets()
+    {
+        $constraint = new Recaptcha();
+        $this->assertNotEquals(Constraint::CLASS_CONSTRAINT, $constraint->getTargets());
+    }
+}
