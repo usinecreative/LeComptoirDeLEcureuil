4d6b5f5db38c275a2739443a14c03380361cab58
Arnaud Frézet <arnaudfrezet@gmail.com>
2017-01-04 23:54:12 +0100

    add method to find User that should be notified on new Comment and a method to remove this subscription

--- src/BlueBear/CmsBundle/Repository/CommentRepository.php
+++ src/BlueBear/CmsBundle/Repository/CommentRepository.php
@@ -2,13 +2,14 @@
 
 namespace BlueBear\CmsBundle\Repository;
 
+use BlueBear\CmsBundle\Entity\Comment;
 use DateTime;
 use LAG\AdminBundle\Repository\DoctrineRepository;
 
 class CommentRepository extends DoctrineRepository
 {
     /**
-     * Return all comments created after $date
+     * Return all comments created after $date.
      *
      * @param DateTime $date
      * @return array
@@ -21,5 +22,51 @@ class CommentRepository extends DoctrineRepository
             ->setParameter('date', $date)
             ->getQuery()
             ->getResult();
+    }
+    
+    /**
+     * Find all Comments from an Article that should be notified in case of new Comment.
+     *
+     * @param Comment $comment
+     *
+     * @return Comment[]
+     */
+    public function findShouldBeNotified(Comment $comment)
+    {
+        return $this
+            ->createQueryBuilder('comment')
+            ->where('comment.article = :article')
+            ->andWhere('comment.notifyNewComments = :notify')
+            ->andWhere('comment.id != :id')
+            ->setParameter('article', $comment->getArticle()->getId())
+            ->setParameter('notify', true)
+            ->setParameter('id', $comment->getId())
+            ->getQuery()
+            ->getResult();
+    }
+    
+    /**
+     * Unsubscribe an email from the notifications send on new Comment.
+     *
+     * @param string $articleSlug
+     * @param string $email
+     */
+    public function unsubscribe($articleSlug, $email)
+    {
+        /** @var Comment[] $comments */
+        $comments = $this
+            ->createQueryBuilder('comment')
+            ->innerJoin('comment.article', 'article')
+            ->where('article.slug = :slug')
+            ->andWhere('comment.authorEmail = :email')
+            ->setParameter('slug', $articleSlug)
+            ->setParameter('email', $email)
+            ->getQuery()
+            ->getResult();
+    
+        foreach ($comments as $comment) {
+            $comment->setNotifyNewComments(false);
+            $this->save($comment);
+        }
     }
 }
