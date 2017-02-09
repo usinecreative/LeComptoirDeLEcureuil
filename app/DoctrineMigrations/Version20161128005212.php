1e6e05b532813443b20babb5f3ab14afc5eed8d4
Arnaud Frézet <arnaudfrezet@gmail.com>
2017-02-09 15:25:42 +0100

    comment code instead of whole migrations

--- app/DoctrineMigrations/Version20161128005212.php
+++ app/DoctrineMigrations/Version20161128005212.php
@@ -1,34 +1,34 @@
 <?php
-//
-//namespace Application\Migrations;
-//
-//use Doctrine\DBAL\Migrations\AbstractMigration;
-//use Doctrine\DBAL\Schema\Schema;
-//use Exception;
-//use JK\CmsBundle\Entity\Media;
-//use PDO;
-//use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-//use Symfony\Component\DependencyInjection\ContainerInterface;
-//use Symfony\Component\Filesystem\Filesystem;
-//
-///**
-// * Change Media table due to new entity.
-// */
-//class Version20161128005212 extends AbstractMigration implements ContainerAwareInterface
-//{
-//    /**
-//     * @var ContainerInterface
-//     */
-//    protected $container;
-//
-//    /**
-//     * @param Schema $schema
-//     */
-//    public function up(Schema $schema)
-//    {
-//        // this up() migration is auto-generated, please modify it to your needs
-//        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-//
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Exception;
+use JK\CmsBundle\Entity\Media;
+use PDO;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * Change Media table due to new entity.
+ */
+class Version20161128005212 extends AbstractMigration implements ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
 //        $this->addSql('ALTER TABLE cms_media ADD updatedAt DATETIME NOT NULL, DROP updated_at, CHANGE filepath fileType VARCHAR(255) NOT NULL, CHANGE created_at createdAt DATETIME NOT NULL');
 //        $this->addSql('ALTER TABLE cms_media ADD description LONGTEXT DEFAULT NULL');
 //
@@ -44,23 +44,23 @@
 //        $this->addSql('ALTER TABLE cms_media CHANGE size size INT DEFAULT NULL');
 //
 //        $this->addSql('ALTER TABLE cms_category DROP thumbnail_name');
-//    }
-//
-//    public function postUp(Schema $schema)
-//    {
-//        parent::postUp($schema);
-//
-//        //$this->updateArticleThumbnails();
-//    }
-//
-//    /**
-//     * @param Schema $schema
-//     */
-//    public function down(Schema $schema)
-//    {
-//        // this down() migration is auto-generated, please modify it to your needs
-//        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-//
+    }
+
+    public function postUp(Schema $schema)
+    {
+        parent::postUp($schema);
+
+        //$this->updateArticleThumbnails();
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
 //        $this->addSql('ALTER TABLE cms_media ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME DEFAULT NULL, DROP createdAt, DROP updatedAt, CHANGE filetype filepath VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
 //        $this->addSql('ALTER TABLE cms_media DROP description');
 //        $this->addSql('ALTER TABLE cms_category DROP FOREIGN KEY FK_6CA2D53CFDFF2E92');
@@ -71,21 +71,21 @@
 //        $this->addSql('DROP INDEX IDX_5CD60177FDFF2E92 ON cms_article');
 //        $this->addSql('ALTER TABLE cms_article ADD thumbnail_name VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci, DROP thumbnail_id');
 //        $this->addSql('ALTER TABLE cms_category ADD thumbnail_name VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci');
-//    }
-//
-//    /**
-//     * Sets the container.
-//     *
-//     * @param ContainerInterface|null $container A ContainerInterface instance or null
-//     */
-//    public function setContainer(ContainerInterface $container = null)
-//    {
-//        $this->container = $container;
-//    }
-//
-//    protected function updateArticleThumbnails()
-//    {
-//        // update existing thumbnails
+    }
+
+    /**
+     * Sets the container.
+     *
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    protected function updateArticleThumbnails()
+    {
+        // update existing thumbnails
 //        $entityManager = $this
 //            ->container
 //            ->get('doctrine.orm.entity_manager');
@@ -126,12 +126,12 @@
 //
 //            $fileSystem->copy($file, $newThumbnailDirectory.$article['thumbnail_name']);
 //        }
-//    }
-//
-//    protected function getFileExtension($file)
-//    {
-//        $array = explode('.', $file);
-//
-//        return array_pop($array);
-//    }
-//}
+    }
+
+    protected function getFileExtension($file)
+    {
+        $array = explode('.', $file);
+
+        return array_pop($array);
+    }
+}
