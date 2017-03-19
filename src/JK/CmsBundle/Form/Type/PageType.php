fb655aa88ecfb8bd59a0b5296330113ef8419234
Arnaud Frézet <arnaudfrezet@gmail.com>
2017-03-19 22:08:29 +0100

    update dependencies

--- src/JK/CmsBundle/Form/Type/PageType.php
+++ src/JK/CmsBundle/Form/Type/PageType.php
@@ -6,7 +6,6 @@ use JK\CmsBundle\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
