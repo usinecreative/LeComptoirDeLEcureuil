610b8a70fa9b1577a957b063a91bd7f31a071c0e
Arnaud Frézet <arnaudfrezet@gmail.com>
2017-06-05 13:34:11 +0200

    fix composer installation on php 5.5.9

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,8 +14,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Symfony\Bundle\WebServerBundle\WebServerBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
 
             // BlueBear
             new LAG\AdminBundle\LAGAdminBundle(),
