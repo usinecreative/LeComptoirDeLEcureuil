dcbf7831b7b79f5a20e20b20a552b0306a08dfb2
johnkrovitch <arnaudfrezet@gmail.com>
2018-09-28 19:04:51 +0200

    Remove unused imports

--- src/Repository/ContentRepository.php
+++ src/Repository/ContentRepository.php
@@ -2,10 +2,6 @@
 
 namespace App\Repository;
 
-use App\Entity\Content;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
-
 class ContentRepository extends AbstractRepository
 {
 }
