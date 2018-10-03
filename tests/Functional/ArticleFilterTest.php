43c3f1b8de99c6caca7c3e0e887b3e3562dea059
johnkrovitch <arnaudfrezet@gmail.com>
2018-10-03 23:27:31 +0200

    Initialize unit testing

---
+++ tests/Functional/ArticleFilterTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class ArticleFilterTest extends WebTestCase
+{
+    public function testFilter()
+    {
+        $this->assertTrue(true);
+    }
+}
